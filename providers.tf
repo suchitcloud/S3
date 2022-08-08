c56335cbf60304257319730353a3a91930a9baad
suchitgawali <95976282+suchitgawali@users.noreply.github.com>
2022-08-08 12:51:34 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -7,8 +7,7 @@ terraform {
       source  = "hashicorp/aws"
     }
     
-provider 
-    "aws" {
+provider "aws" {
    region     = "us-east-1"
    access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
    secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
