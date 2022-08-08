6ffba34750455b59e738d64cd5d9a00d8eebf63b
suchitgawali <95976282+suchitgawali@users.noreply.github.com>
2022-08-08 13:15:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,4 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "private"
 }
 
-provider "aws" {
-   region     = "us-east-1"
-   access_key = "AKIA377IT3VNY75K6B72"
-   secret_key = "UsH+6CKdb8qxtyk/sCvdrtuCs/N9DrVxlx5BS3t3"
-    } 
  
