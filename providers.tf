38873492d6a5373dee09790a6fbf9cf36b54587f
suchitgawali <95976282+suchitgawali@users.noreply.github.com>
2022-08-08 12:54:31 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,8 +9,8 @@ terraform {
     
 provider "aws" {
    region     = "us-east-1"
-   access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-   secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+   access_key = "AKIA377IT3VNY75K6B72"
+   secret_key = "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"
     } 
   }
 }
