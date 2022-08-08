9e5a81affdf0628e46ced99e75ddd1da0eff388e
suchitgawali <95976282+suchitgawali@users.noreply.github.com>
2022-08-08 13:18:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,8 +3,8 @@ terraform {
     bucket = "tf-backend-gitaction"
     region = "us-east-1"
     key = "yourkey/backend-gitaction.tfstate"
-    dynamodb_table = "your-lock-table"
-    encrypt = true
+    #dynamodb_table = "your-lock-table"
+    #encrypt = true
     access_key = "AKIA377IT3VNY75K6B72"
     secret_key = "UsH+6CKdb8qxtyk/sCvdrtuCs/N9DrVxlx5BS3t3"
   }
